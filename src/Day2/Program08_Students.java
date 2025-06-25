68e7de2ebbf166c1cc61769d7b9e5420abd1bbf2
“aiswarya.gopal” <aiswarya.gopal@bounteous.com>
2025-06-25 06:52:12 +0530

    Day 2 - Day 1-6 days automation challenge

---
+++ src/Day2/Program08_Students.java
@@ -0,0 +1,29 @@
+package Day2;
+
+import java.sql.SQLOutput;
+
+public class Program08_Students {
+    int rollNum;
+    String name;
+    int marks;
+    //all 3 is data members
+    void display(){//this is a member function
+        System.out.println(rollNum);
+        System.out.println(name);
+        System.out.println(marks);
+    }
+
+    public static void main(String[] args) {
+        Program08_Students ob = new Program08_Students();
+        ob.rollNum=11;
+        ob.name="Aiswarya";
+        ob.marks=100;
+        ob.display();//To get the output
+
+        Program08_Students ob1 = new Program08_Students();
+        ob1.rollNum=22;
+        ob1.name="Sidhu";
+        ob1.marks=99;
+        ob1.display();
+    }
+}
